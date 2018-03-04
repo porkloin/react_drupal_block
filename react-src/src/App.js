e230f7ca3aa34d1f90ac998bccac4f6144a1a75c
Ryan Bateman <rybateman@gmail.com>
2018-03-03 15:25:47 -0900

    added views config for serializer view in distribution with module (maybe switch to JSON API later?) and more context info listed on block by default

--- react-src/src/App.js
+++ react-src/src/App.js
@@ -3,6 +3,37 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      nodeContent: []
+    }
+    const baseUrl = '/';
+    //const baseUrl = 'http://react-drupal-block.lndo.site/';
+    const path = 'api/content/' + this.props.DrupalNodeUUID;
+    const getAndSetContent = (link) => {
+      fetch(link)
+      .then((response) => {
+        return response.ok ? response.json() : Promise.reject(response.statusText);
+      })
+      .then(document => {
+        console.log(document);
+        this.setState(prevState => ({
+          nodeContent: [...document]
+        }));
+      })
+      .then(() => {
+        this.setState(prevState => ({
+          loading: false
+        })
+      )})
+      .catch(console.log);
+    }
+    if (this.props.DrupalNodeUUID) {
+      getAndSetContent(`${baseUrl}${path}`);
+    }
+  }
+
   render() {
     return (
       <div className="App">
@@ -21,6 +52,18 @@ class App extends Component {
         <p>
           {this.props.DrupalNodeUUID ? this.props.DrupalNodeUUID : 'No UUID found! Are you running this in a Drupal block, or on a local yarn dev server?'}
         </p>
+        <p>
+        Using a JSON view, here are some vital stats about the node we are placed on:
+        </p>
+        { this.state.nodeContent.map((content) => {
+          return (
+            <div>
+              <div>NID: {content.nid[0].value}</div>
+              <div>Title: {content.title[0].value}</div>
+              <div>Body: {content.body[0].value}</div>
+            </div>
+          );
+        })}
       </div>
     );
   }
