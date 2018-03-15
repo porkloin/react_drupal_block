8b862935e1c0b1ca53bb6b02d2c2f28b790bf79f
Ryan Bateman <rybateman@gmail.com>
2018-03-15 14:21:01 -0800

    using window onload to prevent react from trying to load before dom is finished loading

--- react-src/src/index.js
+++ react-src/src/index.js
@@ -4,10 +4,10 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-//attempt to snag UUID that got injected into our twig template.
-var uuid = document.getElementById('react').dataset.uuid;
-var bundle = document.getElementById('react').dataset.bundle;
-
-
-ReactDOM.render(<App DrupalNodeUUID={uuid} DrupalBundle={bundle} />, document.getElementById('react'));
-registerServiceWorker();
+window.onload = function() {
+  //attempt to snag UUID that got injected into our twig template.
+  var uuid = document.getElementById('react').dataset.uuid;
+  var bundle = document.getElementById('react').dataset.bundle;
+  ReactDOM.render(<App DrupalNodeUUID={uuid} DrupalBundle={bundle} />, document.getElementById('react'));
+  registerServiceWorker();
+}
